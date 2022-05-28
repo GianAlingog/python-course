6ce3288e51789d5baba0cb4eae7db9bc2856117e
GianAlingog <76099610+GianAlingog@users.noreply.github.com>
2022-05-28 18:59:46 +0800

    stopped at 55:04

--- tutorial001.py
+++ tutorial001.py
@@ -1,14 +1,14 @@
-# 1
+# 1 Hello World
 print("Hello World!")
 
-# 2
+# 2 Print Statement
 print("Line 1")
 print("Line 2")
 
-# 3
+# 3 Operation on string
 print("*" * 10)
 
-# 4
+# 4 Variables
 my_num = 1 # int / integer
 print(my_num)
 
@@ -25,7 +25,7 @@ patient_name = "John Smith"
 patient_age = "20"
 patient_is_new = True
 
-# 8
+# 8 Input
 # your_name = input("Enter your name: ")
 # print("Hi, " + your_name + "!")
 
@@ -33,7 +33,7 @@ patient_is_new = True
 # your_color = input("What is your favorite color? ")
 # print(your_name + " likes " + your_color)
 
-# 10
+# 10 # Variable conversion
 # birth_year = int(input("Birth Year: ")) # int() to convert variable
 # age = 2022 - birth_year
 # print("You are " + str(age) + " years old.")
@@ -43,7 +43,7 @@ patient_is_new = True
 # weight_kgs = weight_lbs * 0.453592
 # print(str(weight_kgs) + "kgs") # str() to convert string
 
-# 12
+# 12 Tricks with strings
 my_str1 = "str's"
 my_str2 = 'str"s"'
 my_str3 = """
--- tutorial002.py
+++ tutorial002.py
@@ -1,4 +1,4 @@
-# 13
+# 13 Indexes
 my_str1 = "This is my string."
 #          ^    ^         ^ ^
 #          0    5        -3 -1 
@@ -8,4 +8,36 @@ print(my_str1[5:]) # from index 5 until the end
 print(my_str1[:-3]) # from index 0 to -3
 another_str1 = my_str1[:] # start index to end index
 
-# 14
+# 14 Formatted Strings
+first = "t"
+last = "a"
+# message = first + " [" + last + "] is m" # Bad Way
+message = f"{first} [{last}] is m" # Good Way using formatted strings with syntax f""
+print(message)
+
+# 15 String Methods
+my_str3 = "asdfkjapejapefjajpsiefijasdf"
+print(len(my_str3)) # length
+# my_str3.upper()
+# my_str3.lower()
+# my_str3.title()
+# my_str3.find("a") # find index of char / snippet
+# my_str3.replace("a", "b") # find and replace
+# "asdf" in my_str3 # returns a boolean
+# "asdfafskf" in my_str3
+
+# 16 Arithmetic Operations
+print(10 + 3) # addition
+print(10 - 3) # subtraction
+print(10 * 3) # multiplication
+print(10 / 3) # division
+print(10 // 3) # floor division
+print(10 % 3) # modulus
+print(10 ** 3) # power of
+
+x = 10
+x += 3 # same as x = x + 3
+       # also works with -= *= /= etc.
+
+       # In Python, the order of operations is applied.
+print(x)
